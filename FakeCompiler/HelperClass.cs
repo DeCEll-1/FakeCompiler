c9d3a65f46dc0b48026f469e03fc5c25328e401b
Arda Eren <Poedx2@gmail.com>
2023-03-06 22:52:39 +0300

    Initial Relase.

---
+++ FakeCompiler/HelperClass.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Compression;
+
+namespace FakeCompiler
+{
+    internal class HelperClass
+    {
+
+        public string CopyTheFile(string sourceFolder, string targetFolder)
+        {
+            //lmao chatgpt is epic as fuck use it lolllllllllllllllll
+            //look this if you are here https://youtu.be/oHAwhZBnI1A
+
+
+            string newExtension = ".class";
+
+            // Copy the folder and its contents
+            Directory.CreateDirectory(targetFolder);
+            foreach (string dirPath in Directory.GetDirectories(sourceFolder, "*", SearchOption.AllDirectories))
+                Directory.CreateDirectory(dirPath.Replace(sourceFolder, targetFolder));
+            foreach (string filePath in Directory.GetFiles(sourceFolder, "*.*", SearchOption.AllDirectories))
+            {
+                byte[] fileContents = File.ReadAllBytes(filePath);
+                string newFilePath = filePath.Replace(sourceFolder, targetFolder);
+                File.WriteAllBytes(newFilePath, fileContents);
+            }
+
+            // Change the file extensions
+            foreach (string filePath in Directory.GetFiles(targetFolder, "*.*", SearchOption.AllDirectories))
+            {
+                string newFilePath = Path.ChangeExtension(filePath, newExtension);
+                File.Move(filePath, newFilePath);
+            }
+
+            // Zip the folder
+            ZipFile.CreateFromDirectory(targetFolder, targetFolder + ".jar");
+
+
+            return "Completed.";
+        }
+
+        static void CopyFolder(string sourceFolder, string targetFolder)
+        {
+            // Copy all files in the source folder to the target folder
+            string[] files = Directory.GetFiles(sourceFolder);
+            foreach (string file in files)
+            {
+                string fileName = Path.GetFileName(file);
+                string targetPath = Path.Combine(targetFolder, fileName);
+                File.Copy(file, targetPath, true); // overwrite existing files
+            }
+
+            // Recursively copy all subfolders in the source folder to the target folder
+            string[] folders = Directory.GetDirectories(sourceFolder);
+            foreach (string folder in folders)
+            {
+                string folderName = Path.GetFileName(folder);
+                string targetPath = Path.Combine(targetFolder, folderName);
+                CopyFolder(folder, targetPath); // recursively copy subfolders
+            }
+        }
+    }
+}
---
+++ FakeCompiler/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeCompiler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //https://asciiflow.com/#/ for making the ascii menu
+
+            Console.WriteLine("+---------------------------------------------------------------------------------------+\r\n| Welcome to the Fake Compiler.                                                         |\r\n|                                                                                       |\r\n| Due to the limitations of \"security\" this app has opened a file in your C:\\ directory.|\r\n|                                                                                       |\r\n| Please copy paste your scr folders content to the \"C:\\FakeComp\\Source\" directory.     |\r\n|                                                                                       |\r\n| The \"jar\" folder that appears in the \"C:\\FakeComp\" can be ignored.                    |\r\n|                                                                                       |\r\n| the \"C:\\FakeComp\" file will be deleted when you reopen this app.                      |\r\n|                                                                                       |\r\n| Made by DeCell#7556 with THE FUCKING HATE OF INTELLIJS COMPILER                       |\r\n|                                                                                       |\r\n+---------------------------------------------------------------------------------------+");
+
+            DirectoryInfo dimain = new DirectoryInfo("C:\\FakeComp");
+
+            DirectoryInfo disrc = new DirectoryInfo("C:\\FakeComp\\Source");
+
+
+            if (!dimain.Exists)
+            {
+                dimain.Create();
+
+                disrc.Create();
+            }
+            else
+            {
+                dimain.Delete(true);
+
+                dimain.Create();
+
+                disrc.Create();
+            }
+            if (!disrc.Exists)
+            {
+                disrc.Create();
+            }
+
+            string srcpath = "C:\\FakeComp\\Source";
+
+            string targetpath = "C:\\FakeComp\\jar";
+
+            Console.WriteLine("/!\\ be sure that you putten your src to the said folder.");
+
+            Console.WriteLine("Press enter to proceed");
+
+            Console.ReadLine();
+
+            Console.WriteLine(new HelperClass().CopyTheFile(srcpath, targetpath));
+
+            Console.WriteLine("Press enter to close the app");
+
+            Console.ReadLine();
+
+        }
+    }
+}
